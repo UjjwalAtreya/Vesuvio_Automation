6047377dc82bb752779d5d8a34a3d1bc2eed9e75
UJJWAL MANGAL ATREYA <ujjwalmangalatreya@UJJWALs-MacBook-Pro.local>
2023-02-17 09:59:37 +0545

    TestNg

--- src/main/java/OrangeHRMLoginMainMethod.java
+++ src/main/java/OrangeHRMLoginMainMethod.java
@@ -1,25 +1,41 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
 
 public class OrangeHRMLoginMainMethod {
 
-    public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = WebDriverManager.chromiumdriver().create();
+    WebDriver driver;
+    @BeforeMethod
+    public void setup() throws InterruptedException {
+        // Setting up webdriver and base url
+        driver = WebDriverManager.chromiumdriver().create();
         driver.manage().window().maximize();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         Thread.sleep(5000);
-        driver.findElement(By.name("username")).sendKeys();
-        driver.findElement(By.name("password")).sendKeys();
-        driver.findElement(By.xpath("//button[@type='submit']")).click();
-
-        String pageTile = driver.getTitle();
-        if(pageTile.equalsIgnoreCase("")){
-            System.out.println("Test Passed");
-        }else{
-            System.out.println("Test Failed");
-        }
     }
+
+    @Test
+    public void testToVerifyPageTitle(){
+        //Navigation and Asseertion
+        driver.findElement(By.name("username")).sendKeys("Admin");
+        driver.findElement(By.name("password")).sendKeys("admin123");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        String pageTile = driver.getTitle();
+        Assert.assertEquals(pageTile,"OrangeHRM");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        // Browser quit
+        driver.quit();
+    }
+
+
 }
+
+
